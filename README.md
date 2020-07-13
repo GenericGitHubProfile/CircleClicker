174bf3035e2b51ed2cc7d02d9225f29a3ccc4f70
GenericGitHubProfile <up863207@myport.ac.uk>
2020-07-13 17:43:59 +0100

    Documentation

--- README.md
+++ README.md
@@ -5,3 +5,12 @@ Circle Clicker
 
 - Click on the circles to gain points
 - If there's 50 circles on the screen, you lose
+
+Final version
+--------------
+- Users will select difficulty
+- Game will then start with that difficulty
+- Difficulty controls starting speed of circle spawning
+- Circle spawning will increase over time/as after certain number of circles spawned
+- Speed increase will go until a cap, whereby it will stop
+- Game will display final score after user loses.
--- utilities/game.mjs
+++ utilities/game.mjs
@@ -8,6 +8,7 @@ export class Game {
         this.canvas = canvas;
         this.ctx = this.canvas.getContext('2d');
         this.canvas.id = 'canvas';
+        // These are made into constants for box to spawn circles inside, etc.
         this.WIDTH = width;
         this.HEIGHT = height;
         this.canvas.width = this.WIDTH;
@@ -56,47 +57,75 @@ export class Game {
         this.setupGame();
     }
 
+    /*
+    * Set the game state and setup when to start the game
+    * this will be later be done with an event listener so the user can select the difficulty
+    */
     setupGame() {
         this.gameState = this.GAME_STATES.PREGAME;
         this.selectDifficulty();
+        this.startGame();
     }
 
+    /*
+    * Start the main Game with the set variables
+    * Adds score and no circle counters, so the user can track how well they are doing
+    * clears the main screen and then starts spawning circles and allows them to be clicked
+    */
     startGame() {
         this.gameState = this.GAME_STATES.MAIN;
 
-        this.startSpawningCircles();
+        // Setup scoring and noCircle tracking
         this.ctx.fillStyle = '#000';
         this.ctx.fillText("Score: ", 10, 15);
         this.ctx.fillText("No Circles: ", 10, 35);
         this.displayScore();
         let border = new Square(0,50,this.WIDTH,this.HEIGHT);
         border.draw(this.ctx,this.BG_COL);
+        // Add the interactive componants
+        this.startSpawningCircles();
         this.canvas.addEventListener('click', this.canvasClickEvent.bind(null, this), false);
     }
 
+    /*
+    * When the Number of circles would exceed the max number (currently 50)
+    * wipe the screen and display the final score
+    */
     endGame() {
         this.gameState = this.GAME_STATES.END;
 
+        // Remove interactive componants
         this.stopSpawningCircles();
         this.canvas.removeEventListener('click', this.canvasClickEvent.bind(null, this), false);
 
+        // Clear Screen
         let temp = new Square(0,0, this.canvas.width, this.canvas.height);
         temp.draw(this.ctx, this.BG_COL);
 
+        // Display final score
         this.ctx.font = "60px Arial";
         this.ctx.fillText(`FINAL SCORE ${this.score}`, 200, 300);
     }
 
+    /*
+    * Will setup screen with difficulties to select with event listener and screen region
+    * currently sets the difficulty to medium
+    */
     selectDifficulty() {
         this.difficulty = this.GAME_DIFFICULTY.MEDIUM;
-        this.startGame();
     }
 
+    /*
+    * Increments and displays the score to user
+    */
     increaseScore() {
         this.score++;
         this.displayScore();
     }
 
+    /*
+    * Displays score to user
+    */
     displayScore() {
         this.ctx.clearRect(this.SCORE_COORDS.x, this.SCORE_COORDS.y-15, 100, 20);
         this.ctx.fillStyle = '#000';
@@ -113,16 +142,25 @@ export class Game {
         return this.noCircles;
     }
 
+    /*
+    * Displays number of circles to user
+    */
     displayNoCircles() {
         this.ctx.fillStyle = ((this.noCircles<this.MAX_CIRCLES*0.6) ? this.CIRCLES_SAFE : this.CIRCLES_WARN);
         this.ctx.clearRect(this.NOCIRCLES_COORDS.x, this.NOCIRCLES_COORDS.y-15, 100, 20);
         this.ctx.fillText(`${this.noCircles}`, this.NOCIRCLES_COORDS.x, this.NOCIRCLES_COORDS.y);
     }
 
+    /*
+    * Adds interval for spawning circles, interval based on difficulty selected
+    */
     startSpawningCircles() {
         this.spawning = setInterval(() => this.spawnCircles(), this.difficulty);
     }
 
+    /*
+    * Spawns circles and handles whether the user has lost or not
+    */
     spawnCircles() {
         if(this.shapeArr.length >= this.MAX_CIRCLES) {
             // LOSE CONDITION
@@ -135,16 +173,28 @@ export class Game {
         this.updateNoCircles(1);
     }
 
+    /*
+    * Stops the circles from spawning
+    */
     stopSpawningCircles() {
         clearInterval(this.spawning);
     }
 
+    /*
+    * Function for event listener of clicking on the canvas
+    * checks whether a circle is within distance, if so removes it and redraws the screen
+    */
     canvasClickEvent(obj, e) {
-        const RECT = canvas.getBoundingClientRect();
+        // Get bounding box of Canvas element
+        const RECT = obj.canvas.getBoundingClientRect();
+        // Get the X from the left side of the canvas, rather than left side of screen
         const X = e.clientX - RECT.left;
+        // Get the Y from the top of the canvas, rather than the top of the screen
         const Y = e.clientY - RECT.top;
+        // Iterate through all circles
         obj.shapeArr.forEach((item, i) => {
             let dist = obj.getDistance(item.x,item.y,X,Y);
+            // If current circle is whitin distance, remove it, give score, and reduce number of circles, then redraw screen
             if(dist < obj.CIRCLE_DIST) {
                 obj.increaseScore();
                 obj.updateNoCircles(-1);
@@ -154,6 +204,9 @@ export class Game {
         });
     }
 
+    /*
+    * Clears screen then redraws all circles
+    */
     redraw() {
         let temp = new Square(0,50, this.canvas.width, this.canvas.height);
         temp.draw(this.ctx, this.BG_COL);
@@ -163,14 +216,23 @@ export class Game {
 
     }
 
+    /*
+    * Returns a random number between the given min and max parameters
+    */
     randNo(min, max) {
         return Math.floor(Math.random() * max) + min;
     }
 
+    /*
+    * Returns a random hex colour
+    */
     randCol() {
         return '#' + ('000000'+(Math.random()*(1<<24)|0).toString(16)).slice(-6);
     }
 
+    /*
+    * Returns the distance between two given points
+    */
     getDistance(x1,y1,x2,y2) {
         return Math.sqrt(Math.pow(x1-x2,2) + Math.pow(y1-y2,2));
     }
